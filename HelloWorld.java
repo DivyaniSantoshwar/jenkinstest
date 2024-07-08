16763559318782034c42941ab8e4d15ea11b89b1
DivyaniSantoshwar <diyasantoshwar95@gmail.com>
2024-07-08 18:06:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args){
        System.out.println("Hello World!");
-<<<<<<< HEAD
-       System.out.println("HI")
-=======
-       System.out.println("Hello!");
->>>>>>> 7e03a858cb94d93b2fa9a6c120df3a17f4a6aa96
+       System.out.println("HI");
+       System.out.println("Tata Byebye!");
     }
 }
